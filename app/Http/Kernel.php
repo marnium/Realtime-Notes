2f7bd78a172de9ce3de46f4a5b5ceebdcdfea62b
Martín Monjaraz Almaraz <marnium97@gmail.com>
2021-06-04 07:30:07 -0500

    Eliminacion del middleware Https

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
-            \App\Http\Middleware\HttpsProtocolMiddleware::class
+            //\App\Http\Middleware\HttpsProtocolMiddleware::class
         ],
 
         'api' => [
